8c4b1fcbd1e831c17cbffd8c4e28ba1cdf6999c3
DeanS <20067691@mail.wit.ie>
2023-12-19 15:51:00 +0000

    Error message on Sign up & Login

--- movies/src/pages/signUpPage.js
+++ movies/src/pages/signUpPage.js
@@ -7,6 +7,7 @@ const SignUpPage = props => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [passwordAgain, setPasswordAgain] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // Added state for error message
   const [registered, setRegistered] = useState(false);
 
   const register = () => {
@@ -16,6 +17,9 @@ const SignUpPage = props => {
     if (validPassword && password === passwordAgain) {
       context.register(userName, password);
       setRegistered(true);
+    }
+    else {
+      setErrorMessage("Invalid password. Password must be at least 8 characters long and contains at least one characters, digit, and special character.");
     }
   }
 
@@ -27,6 +31,9 @@ const SignUpPage = props => {
     <div style={{ display: 'flex', justifyContent: 'center',  minHeight: '100vh', marginBottom: '20px' }}>
             <div>
       <h2>SignUp page</h2>
+      {errorMessage && (
+          <p style={{ color: 'red' }}>{errorMessage}</p>
+        )}
       <p>You must register a username and password to log in </p>
       <input value={userName} placeholder="user name" onChange={e => {
         setUserName(e.target.value);
